2f4f20934cc6ad8e8a15a6eaf44c414c37179e6f
busfahrer <eggdropfan@yahoo.com>
2019-12-03 19:08:24 +0100

    use var for creation count

--- create.sh
+++ create.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
+COUNT=3
+
 multipass launch --name node1
 multipass exec node1 -- sh -c "curl -sfL https://get.k3s.io | sh -"
 
 TOKEN=`multipass exec node1 -- sudo cat /var/lib/rancher/k3s/server/node-token`
 IP=`multipass list --format csv | grep "^node1" | cut -d, -f3`
 
-for i in `seq 2 $1`
+for i in `seq 2 $COUNT`
 do
   multipass launch --name node$i
   multipass exec node$i -- sh -c "curl -sfL https://get.k3s.io | K3S_URL=https://$IP:6443 K3S_TOKEN=$TOKEN sh -"
